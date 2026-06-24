5178b1197aca57b3d5ba86a8bbf6699f21e73543
Sanjeev Kumar <kumarsanjeevkumar64@gmail.com>
2026-06-24 22:04:56 +0530

    Add files via upload

---
+++ traffic_signal.py
@@ -0,0 +1,14 @@
+"""
+Question-> Identify traffic signal meaning. 
+
+light = input("Enter the light color: ")
+if light=="red":
+    print("Stop!")
+elif light=="yello":
+    print("Walk")
+elif light=="green":
+    print("Go")
+else:
+    print("Light is broken!")
+
+"""
