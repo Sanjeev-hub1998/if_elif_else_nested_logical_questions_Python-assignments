98ca14a8fd0c8031e2fd62d7c0288f2501cc48f0
Sanjeev Kumar <kumarsanjeevkumar64@gmail.com>
2026-06-24 22:04:11 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,12 @@
+"""
+Question-> Categorize temperature as Cold / Warm / Hot. 
+
+tem = int(input("Enter the temperature: "))
+if tem>=32:
+    print("Hot")
+elif tem>=15 and tem<32:
+    print("Warm")
+else:
+    print("Cold")
+
+"""
